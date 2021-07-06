50d7a59959cb4069108d9644c0f79341fe65204a
Pallav <contrazap@gmail.com>
2021-07-06 14:05:26 +0530

    Making About class

---
+++ webapp/about.py
@@ -0,0 +1,23 @@
+import justpy as jp
+
+
+class About:
+    path = "/about"
+
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+        jp.Div(a=div,
+               text="This is the about page!",
+               classes="text-4xl m-2")
+        jp.Div(a=div,
+               text="""
+               This is a webapp for a dictionary.
+               Created as part of practice of the udemy course, Advanced Python by Example.
+               """,
+               classes="text-lg")
+        return wp
+
+
+jp.Route(About.path, About.serve)
+jp.justpy(port=8001)
